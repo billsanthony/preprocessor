728709169ac1417ec596390b5e316ae67db2c44c
billsanthony <anthonypaulakpan@gmail.com>
2022-10-16 08:39:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,14 +5,9 @@
  *  * main - Entry Point 
  *   * Return: Always 0 (Success)                                                                                           
  *
- *    */                                                                                                                     
-
-int main(void)                                                                                                          
-
-{                                                                                                                       
-
-	        puts("\"Programming is like building a multilingual puzzle");                                                   
-
-		        return (0);                                                                                                     
-
-}                                 
+ *    */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
+}
